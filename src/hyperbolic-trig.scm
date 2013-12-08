498ba8b956932c58f51ad2e46017acdf01bc4419
pqnelson <pqnelson@gmail.com>
2013-12-08 09:29:04 -0800

    [minor] Added more tests

--- src/hyperbolic-trig.scm
+++ src/hyperbolic-trig.scm
@@ -136,12 +136,17 @@
 
 (log/info "\nDefining :golden-ratio")
 (define :golden-ratio (/ (+ 1 (sqrt 5)) 2))
-(assert (float= (/ 1 :golden-ratio)
-                (- :golden-ratio 1)))
 (define :ln-phi (ln :golden-ratio))
-
 (define (phi-cf k)
   (cont-frac (lambda (i) 1) k))
+
+;; various identities the golden ratio satisfies...
+(assert (float= (/ 1 :golden-ratio)
+                (- :golden-ratio 1)))
+(assert (float= (+ (square :golden-ratio) 
+                   (/ 1 :golden-ratio)) 
+                (* 2 :golden-ratio)))
+(assert (< :ln-phi 1/2))
 (assert (float= (phi-cf 38) :golden-ratio))
 (assert (not (float= (phi-cf 36) :golden-ratio)))
 
